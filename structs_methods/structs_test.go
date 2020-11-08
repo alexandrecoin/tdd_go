e81e8236e193effbab1433ecae7ac47ce9b4c0e8
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-08 17:57:21 +0100

    Refactor TestArea using interface

--- structs_methods/structs_test.go
+++ structs_methods/structs_test.go
@@ -12,10 +12,24 @@ func TestPerimeter(t * testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 12.0}
-	result := Area(rectangle)
-	expectedResult := 120.0
-	if expectedResult != result {
-		t.Errorf("Expected %.2f but got %.2f", expectedResult, result)
-	}
+
+	checkArea := func(t *testing.T, shape Shape, expectedResult float64) {
+		t.Helper()
+		result := shape.Area()
+		if result != expectedResult{
+			t.Errorf("Expected %g but got %g", expectedResult, result)
+		}
+	} 
+
+	t.Run("Calculate area of a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 12.0}
+		expectedResult := 120.0
+		checkArea(t, rectangle, expectedResult)
+	})
+
+	t.Run("Calculate the area of a circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		expectedResult := 314.1592653589793
+		checkArea(t, circle, expectedResult)
+	})
 }
