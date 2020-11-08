517886d07a85de1c5391e75c222ee54aca770cca
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-08 15:27:28 +0100

    Refactor tests for structs usage

--- structs_methods/structs_test.go
+++ structs_methods/structs_test.go
@@ -3,16 +3,18 @@ package main
 import "testing"
 
 func TestPerimeter(t * testing.T) {
-	result := Perimeter(4, 2)
-	expectedResult := 12.0
+	rectangle := Rectangle{10.0, 12.0}
+	result := Perimeter(rectangle)
+	expectedResult := 44.0
 	if expectedResult != result {
 		t.Errorf("Expected %.2f but got %.2f", expectedResult, result)
 	}
 }
 
 func TestArea(t *testing.T) {
-	result := Area(4, 2)
-	expectedResult := 8.0
+	rectangle := Rectangle{10.0, 12.0}
+	result := Area(rectangle)
+	expectedResult := 120.0
 	if expectedResult != result {
 		t.Errorf("Expected %.2f but got %.2f", expectedResult, result)
 	}
