868c34e916d18bceaa662b882c5423509c7af188
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-09 06:53:17 +0100

    Use fmt Stringer interface for custom type print return

--- pointers_errors/pointers_errors_test.go
+++ pointers_errors/pointers_errors_test.go
@@ -6,8 +6,8 @@ func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(Bitcoin(10))
 	result := wallet.Balance()
-	expectedResult := Bitcoin(10)
+	expectedResult := Bitcoin(9)
 	if expectedResult != result {
-		t.Errorf("Expected %d but got %d", expectedResult, result)
+		t.Errorf("Expected %s but got %s", expectedResult, result)
 	}
 }
