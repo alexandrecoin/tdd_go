b83062498d7b683da541369c5146e67c958c8f81
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-09 09:52:30 +0100

    Add withdraw method to wallet struct

--- pointers_errors/pointers_errors_test.go
+++ pointers_errors/pointers_errors_test.go
@@ -3,11 +3,24 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	result := wallet.Balance()
-	expectedResult := Bitcoin(9)
-	if expectedResult != result {
-		t.Errorf("Expected %s but got %s", expectedResult, result)
-	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		result := wallet.Balance()
+		expectedResult := Bitcoin(10)
+		if expectedResult != result {
+			t.Errorf("Expected %s but got %s", expectedResult, result)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		wallet.Withdraw(Bitcoin(5))
+		result := wallet.Balance()
+		expectedResult := Bitcoin(5)
+		if expectedResult != result {
+			t.Errorf("Expected %s but got %s", expectedResult, result)
+		}
+	})
 }
