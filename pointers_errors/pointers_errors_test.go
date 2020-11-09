62fb2f7b00b9e4c8d377253cbcd9b78d1aaaf7f6
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-09 06:06:14 +0100

    TDD for pointers and errors

---
+++ pointers_errors/pointers_errors_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(10)
+	result := wallet.Balance()
+	expectedResult := 10
+	if expectedResult != result {
+		t.Errorf("Expected %d but got %d", expectedResult, result)
+	}
+}
