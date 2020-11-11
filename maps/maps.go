0ec246ed7f9e8d49a21965cb8e8c899184b70043
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-11 21:34:26 +0100

    Maps: add AddEntry function and refacto

--- maps/maps.go
+++ maps/maps.go
@@ -4,12 +4,30 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("Could not find the word you are looking for.")
+var (
+	ErrNotFound = errors.New("Could not find the word you are looking for.")
+	ErrWordExists = errors.New("This entry already exists")
+)
 
-func (d Dictionary) Search(word string) (string, error) {
-	definition, isWordFound := d[word]
+func (d Dictionary) Search(entry string) (string, error) {
+	definition, isWordFound := d[entry]
 	if !isWordFound {
 		return "", ErrNotFound
 	}
 	return definition, nil
+}
+
+func (d Dictionary) AddEntry(entry, definition string) error {
+	_, err := d.Search(entry)
+	
+	switch err {
+	case ErrNotFound:
+		d[entry] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	
+	return nil
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -7,7 +7,7 @@ func TestSearch(t *testing.T) {
 	t.Run("known word", func(t *testing.T) {
 		t.Parallel()
 		dictionary := Dictionary{"test": "this is just a test"}
-	
+
 		result, _ := dictionary.Search("test")
 		expectedResult := "this is just a test"
 		assertStrings(t, result, expectedResult)
@@ -22,16 +22,57 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAddEntry(t *testing.T) {
+	t.Run("New word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "entryTest"
+		definition := "This is a test for an entry add"
+		dictionary.AddEntry(word, definition)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("Already existing word", func(t *testing.T) {
+		word := "test"
+		definition := "This is a definition"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.AddEntry(word, "New definition")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+
+	})
+}
+
 func assertStrings(t *testing.T, result, expectedResult string) {
 	t.Helper()
-	if expectedResult != result {
+	if result != expectedResult {
 		t.Errorf("Expected %q but got %q", expectedResult, result)
 	}
 }
 
 func assertError(t *testing.T, result, expectedResult error) {
 	t.Helper()
-	if expectedResult != result {
+
+	if result != expectedResult {
 		t.Errorf("Expected %q but got %q", expectedResult, result)
+	}
+
+	if result == nil {
+		if expectedResult == nil {
+			return
+		}
+		t.Fatal("Expected to get an error")
+	}
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	result, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("Should find added word: ", err)
+	}
+
+	if definition != result {
+		t.Errorf("Expected %q but got %q", definition, result)
 	}
 }
