7442b8beb213aec3a18a721b87b5557dbed436f8
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-11 12:59:01 +0100

    Maps: Replace dict by type

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	result := Search(dictionary, "test")
+	result := dictionary.Search("test")
 	expectedResult := "this is just a test"
 	assertStrings(t, result, expectedResult)
 }
