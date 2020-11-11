39bbdd0476233a49b36ed6274c031e0c02ecee3e
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-11 13:25:58 +0100

    Maps: Add error handling when no word found in map

--- maps/maps.go
+++ maps/maps.go
@@ -1,7 +1,13 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, isWordFound := d[word]
+	if !isWordFound {
+		return "", errors.New("Could not find the word you are looking for.")
+	}
+	return definition, nil
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -3,11 +3,26 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
 
-	result := dictionary.Search("test")
-	expectedResult := "this is just a test"
-	assertStrings(t, result, expectedResult)
+	t.Run("known word", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
+	
+		result, _ := dictionary.Search("test")
+		expectedResult := "this is just a test"
+		assertStrings(t, result, expectedResult)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is another test"}
+		_, err := dictionary.Search("someWord")
+		expectedResult := "Could not find the word you are looking for."
+
+		if err == nil {
+			t.Fatal("Expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), expectedResult)
+	})
 }
 
 func assertStrings(t *testing.T, result, expectedResult string) {
