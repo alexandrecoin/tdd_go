a8f70ba74c5fd84411e6c3eb5f98180062fa1236
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-28 06:24:53 +0100

    Mocking: Add mock spy

--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -7,12 +7,28 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
-	Countdown(buffer)
+	spySleeper := &SpySleeper{}
+	Countdown(buffer, spySleeper)
 
 	result := buffer.String()
-	expectedResult := "3"
+	expectedResult := `3
+2
+1
+Go!`
 
 	if expectedResult != result {
 		t.Errorf("Expected %q but got %q", expectedResult, result)
 	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("Expected 3 sleep calls but got %d", spySleeper.Calls)
+	}
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
 }
